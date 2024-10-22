9a042349c3259bec546489bc2d65ae4ff6d42fbf
laub141 <brianlau5665@gmail.com>
2024-10-22 10:25:42 -0600

    Fixed profile icon and added a new activity for editting profile information

--- Matata/app/src/main/java/com/example/matata/MainActivity.java
+++ Matata/app/src/main/java/com/example/matata/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.matata;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView profileIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        profileIcon = findViewById(R.id.profileIcon);
+
+        profileIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
