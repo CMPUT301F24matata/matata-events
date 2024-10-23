979a94f8d19c90f9ae093f13531851d1e5e9be5f
laub141 <brianlau5665@gmail.com>
2024-10-23 10:59:56 -0600

    Can navigate between main page and edit profile activities

--- Matata/app/src/main/java/com/example/matata/Event.java
+++ Matata/app/src/main/java/com/example/matata/Event.java
@@ -1,12 +1,18 @@
 package com.example.matata;
 
+/**
+ *  This is an implementation of the Event class
+ */
 public class Event {
     private String name;
 
+    /**
+     * Getter and Setters for the class.
+     * @param name
+     */
     public Event(String name) {
         this.name = name;
     }
-
     public String getName() {
         return name;
     }
--- Matata/app/src/main/java/com/example/matata/ProfileActivity.java
+++ Matata/app/src/main/java/com/example/matata/ProfileActivity.java
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Objects;
+
 public class ProfileActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +22,10 @@ public class ProfileActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
     }
 }
