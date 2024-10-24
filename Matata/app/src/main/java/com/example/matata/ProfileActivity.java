aab4beafb081326d7339c1ca8211638d16a9e810
laub141 <brianlau5665@gmail.com>
2024-10-23 21:34:51 -0600

    Added functionality to personal information edit fields

--- Matata/app/src/main/java/com/example/matata/ProfileActivity.java
+++ Matata/app/src/main/java/com/example/matata/ProfileActivity.java
@@ -1,6 +1,10 @@
 package com.example.matata;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +15,8 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.Objects;
 
 public class ProfileActivity extends AppCompatActivity {
+    private EditText nameEditText, phoneEditText, emailEditText;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +28,26 @@ public class ProfileActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        nameEditText = findViewById(R.id.nameEditText);
+        phoneEditText = findViewById(R.id.phoneEditText);
+        emailEditText = findViewById(R.id.emailEditText);
+        saveButton = findViewById(R.id.saveButton);
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get input values
+                String name = nameEditText.getText().toString();
+                String phoneNumber = phoneEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+
+                // Create an Entrants object
+                Entrant entrant = new Entrant(name, phoneNumber, email);
+            }
+        });
     }
 }
