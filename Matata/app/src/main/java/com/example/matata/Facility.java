c7ee2b4eef21a81bba41cd0fa6583f957fa5ce2f
rcli620 <richard164184@gmail.com>
2024-10-27 18:46:27 -0600

    Implemented organizer managing facility

--- Matata/app/src/main/java/com/example/matata/Facility.java
+++ Matata/app/src/main/java/com/example/matata/Facility.java
@@ -1,4 +1,27 @@
 package com.example.matata;
 
 public class Facility {
+    private String name;
+    private String description;
+
+    public Facility(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name, Organizer organizer) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description, Organizer organizer) {
+        this.description = description;
+    }
 }
--- Matata/app/src/main/java/com/example/matata/Organizer.java
+++ Matata/app/src/main/java/com/example/matata/Organizer.java
@@ -1,4 +1,67 @@
 package com.example.matata;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Organizer {
+
+    private List<Facility> myFacilities = new ArrayList<>();
+
+    private String name;
+
+    public Organizer (String name) {
+        this.name = name;
+    }
+
+    public String getName () {
+        return this.name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    private Facility getFacilityByName (String facilityName) {
+        Facility target = null;
+        for (Facility facility : myFacilities) {
+            if (facility.getName().equals(facilityName)) {
+                target = facility;
+            }
+        }
+
+        return target;
+    }
+
+    public List<Facility> getFacilities (Administrator administrator) {
+        return myFacilities;
+    }
+
+    public void createFacility (String name, String description) {
+        Facility target = getFacilityByName(name);
+        if (target == null) {
+            myFacilities.add(new Facility(name, description));
+        } else {
+            System.out.println("Facility name already exist");
+        }
+    }
+
+    public void deleteFacility (Facility facility) {
+        if (!myFacilities.remove(facility)) {
+            System.out.println("Facility not found");
+        }
+    }
+
+    public void updateFacility (Facility facility, String newName, String newDescription) {
+        if (myFacilities.contains(facility)){
+            facility.setName(newName, this);
+            facility.setDescription(newDescription, this);
+        } else {
+            System.out.println("Facility not found");
+        }
+    }
+
+
 }
+
+
