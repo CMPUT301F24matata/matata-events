8ba5b5404bd0dce294c1a4670e5be04d31549f44
laub141 <brianlau5665@gmail.com>
2024-10-30 20:49:44 -0600

    Implemented Event class, Added a RecyclerView to MainActivity and it's accompanying Adapter.

--- Matata/app/src/main/java/com/example/matata/Event.java
+++ Matata/app/src/main/java/com/example/matata/Event.java
@@ -1,24 +1,60 @@
 package com.example.matata;
 
-/**
- *  This is an implementation of the Event class
- */
-public class Event {
-    private String name;
+import java.io.Serializable;
+
+public class Event implements Serializable {
+    private String title;
+    private String date;
+    private String time;
+    private String location;
+    private String description;
+
+    // Constructor
+    public Event(String title, String date, String time, String location, String description) {
+        this.title = title;
+        this.date = date;
+        this.time = time;
+        this.location = location;
+        this.description = description;
+    }
 
     /**
-     * Getter and Setters for the class.
-     * @param name
+     * Getters for the Event class
+     * @return
      */
-    public Event(String name) {
-        this.name = name;
+    public String getTitle() {
+        return title;
     }
-    public String getName() {
-        return name;
+    public String getDate() {
+        return date;
+    }
+    public String getTime() {
+        return time;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public String getDescription() {
+        return description;
     }
 
-
-
-
+    /**
+     * Setters for the Event class
+     * @param title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public void setTime(String time) {
+        this.time = time;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
-
