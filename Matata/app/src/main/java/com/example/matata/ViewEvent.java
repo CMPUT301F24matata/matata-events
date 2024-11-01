af3a831a5264e6b7caa5dbc5d91a357dab2bc00a
shivaansh81001 <shivaanshbhatia007@gmail.com>
2024-10-31 18:41:35 -0600

    updated goback button

--- Matata/app/src/main/java/com/example/matata/ViewEvent.java
+++ Matata/app/src/main/java/com/example/matata/ViewEvent.java
@@ -1,16 +1,23 @@
 package com.example.matata;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ViewEvent extends AppCompatActivity {
-
+    private ImageView goBack;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_event_details);
+
+        goBack=findViewById(R.id.go_back_view_event);
+
+        goBack.setOnClickListener(v->{
+            finish();
+        });
 
 
     }
