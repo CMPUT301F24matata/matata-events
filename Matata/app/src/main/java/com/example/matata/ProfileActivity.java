48513bc0746b5251206324ae222090dcf19ac173
CHIROOTH <chiroothgirigowda@gmail.com>
2024-11-03 06:58:17 -0700

    implemented the inital generated profile for given username

--- Matata/app/src/main/java/com/example/matata/ProfileActivity.java
+++ Matata/app/src/main/java/com/example/matata/ProfileActivity.java
@@ -1,28 +1,40 @@
 package com.example.matata;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.Settings;
+import android.util.Base64;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,15 +43,19 @@ public class ProfileActivity extends AppCompatActivity {
     private ImageView profileIcon;
     private FirebaseFirestore db;
     private String deviceId;
-
-    // Replace with the unique user ID (e.g., FirebaseAuth UID) to identify each user
-    private static String USER_ID = "unique_user_id";
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    private Switch notifications;
+    private String imageUriString;
+    private Uri imageUri;
+    @SuppressLint("HardwareIds")
+    private String USER_ID = "";
 
     private final ActivityResultLauncher<Intent> profilePicLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             result -> {
                 if (result.getResultCode() == RESULT_OK) {
-                    loadProfilePicture();
+                    loadProfilePicture(getSharedPreferences("ProfilePrefs", MODE_PRIVATE)
+                            .getString("profile_image_uri", null));
                 }
             });
 
@@ -48,32 +64,32 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_profile);
 
-        // Initialize Firestore
         db = FirebaseFirestore.getInstance();
 
         USER_ID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
 
-        // Initialize UI elements
         profileIcon = findViewById(R.id.profileIcon);
         nameEditText = findViewById(R.id.nameEditText);
         phoneEditText = findViewById(R.id.phoneEditText);
         emailEditText = findViewById(R.id.emailEditText);
         Button saveButton = findViewById(R.id.saveButton);
         ImageView back = findViewById(R.id.btnBackProfile);
-        Switch notifications = findViewById(R.id.switch_notification);
+        notifications = findViewById(R.id.switch_notification);
+
+        imageUriString = getSharedPreferences("ProfilePrefs", MODE_PRIVATE)
+                .getString("profile_image_uri", null);
+
+        loadProfilePicture(imageUriString);
+
+        loadProfileData();
 
         back.setOnClickListener(v -> finish());
 
-        // Load profile data from Firestore
-        loadProfileData();
-
-        // Set up profile picture click listener
         profileIcon.setOnClickListener(v -> {
             Intent intent = new Intent(ProfileActivity.this, ProfilePicActivity.class);
             profilePicLauncher.launch(intent);
         });
 
-        // Save button click listener to save profile data to Firestore
         saveButton.setOnClickListener(v -> {
             String name = nameEditText.getText().toString().trim();
             String email = emailEditText.getText().toString().trim();
@@ -85,30 +101,28 @@ public class ProfileActivity extends AppCompatActivity {
             } else if (email.isEmpty()) {
                 Toast.makeText(this, "No Email Found", Toast.LENGTH_SHORT).show();
             } else {
-                saveProfileData(name, phoneNumber, email, notificationsChecked);
+                saveProfileData(name, phoneNumber, email, notificationsChecked, imageUriString);
             }
         });
 
-        loadProfilePicture();
     }
 
-    // Method to save profile data to Firestore
-    private void saveProfileData(String name, String phone, String email, boolean notificationsChecked) {
-        // Create a map to hold the user data
+
+    private void saveProfileData(String name, String phone, String email, boolean notificationsChecked, String imageUriStringi) {
+
         Map<String, Object> userProfile = new HashMap<>();
         userProfile.put("username", name);
         userProfile.put("phone", phone);
         userProfile.put("email", email);
         userProfile.put("notifications", notificationsChecked);
+        userProfile.put("profileUri", imageUri);
 
-        // Save the data in the "USER_PROFILES" collection under a document with the user's unique ID
         db.collection("USER_PROFILES").document(USER_ID)
                 .set(userProfile)
                 .addOnSuccessListener(aVoid -> Toast.makeText(ProfileActivity.this, "Profile saved successfully", Toast.LENGTH_SHORT).show())
                 .addOnFailureListener(e -> Toast.makeText(ProfileActivity.this, "Failed to save profile", Toast.LENGTH_SHORT).show());
     }
 
-    // Method to load profile data from Firestore
     private void loadProfileData() {
         DocumentReference docRef = db.collection("USER_PROFILES").document(USER_ID);
         docRef.get()
@@ -117,28 +131,73 @@ public class ProfileActivity extends AppCompatActivity {
                         String name = documentSnapshot.getString("username");
                         String phone = documentSnapshot.getString("phone");
                         String email = documentSnapshot.getString("email");
+                        boolean notificationsChecked = Boolean.TRUE.equals(documentSnapshot.getBoolean("notifications"));
+                        String sImageUri = documentSnapshot.getString("profileUri");
 
-                        // Set the values in the EditTexts
                         nameEditText.setText(name != null ? name : "");
                         phoneEditText.setText(phone != null ? phone : "");
                         emailEditText.setText(email != null ? email : "");
-                    } else {
-                        Toast.makeText(ProfileActivity.this, "No profile found", Toast.LENGTH_SHORT).show();
+                        notifications.setChecked(notificationsChecked);
+                        loadProfilePicture(sImageUri);
+
                     }
+
                 })
                 .addOnFailureListener(e -> Toast.makeText(ProfileActivity.this, "Failed to load profile", Toast.LENGTH_SHORT).show());
     }
 
-    // Load profile picture from SharedPreferences or show initials as a fallback
-    private void loadProfilePicture() {
-        String imageUriString = getSharedPreferences("ProfilePrefs", MODE_PRIVATE)
-                .getString("profile_image_uri", null);
-
+    private void loadProfilePicture(String imageUriString) {
         if (imageUriString != null) {
             Uri imageUri = Uri.parse(imageUriString);
             Glide.with(this).load(imageUri).into(profileIcon);
         } else {
-            profileIcon.setImageResource(R.drawable.ic_upload);
+
+            String un = nameEditText.getText().toString().trim();
+
+            if (un.isEmpty()) {
+                profileIcon.setImageResource(R.drawable.ic_upload);
+            } else {
+                StringBuilder initials = new StringBuilder();
+                String[] words = un.trim().split("\\s+");
+
+                for (String word : words) {
+                    if (!word.isEmpty()) {
+                        initials.append(Character.toUpperCase(word.charAt(0)));
+                    }
+                }
+                String initial = initials.toString();
+
+                imageUri = createImageFromString(this, initial);
+
+                Glide.with(this).load(imageUri).into(profileIcon);
+            }
         }
     }
+
+    public static Uri createImageFromString(Context context, String text) {
+
+        int bitmapWidth = 400;
+        int bitmapHeight = 400;
+        Bitmap bitmap = Bitmap.createBitmap(bitmapWidth, bitmapHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawColor(Color.BLACK);
+
+        Paint paint = new Paint();
+        paint.setColor(Color.WHITE);
+        paint.setTextSize(100);
+        paint.setTextAlign(Paint.Align.CENTER);
+        paint.setAntiAlias(true);
+
+        float textHeight = paint.descent() - paint.ascent();
+        float textOffset = (textHeight / 2) - paint.descent();
+        canvas.drawText(text, (float) bitmapWidth / 2, ((float) bitmapHeight / 2) + textOffset, paint);
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        String base64String = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
+        String imageUri = "data:image/png;base64," + base64String;
+
+        return Uri.parse(imageUri);
+    }
+
 }
