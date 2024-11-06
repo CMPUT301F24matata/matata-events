881c74c450e9a2bae3a5cd28aa78ef86cc51a6bf
rcli620 <richard164184@gmail.com>
2024-11-06 15:12:06 -0700

    Organizer can now save facility profile

--- Matata/app/src/main/java/com/example/matata/ProfileActivity.java
+++ Matata/app/src/main/java/com/example/matata/ProfileActivity.java
@@ -35,6 +35,7 @@ import java.util.Map;
 
 public class ProfileActivity extends AppCompatActivity {
     private EditText nameEditText, phoneEditText, emailEditText;
+    private EditText facilityName, facilityAddress, facilityCapacity, facilityContact, facilityEmail, facilityOwner;
     private ImageView profileIcon;
     private FirebaseFirestore db;
     @SuppressLint("UseSwitchCompatOrMaterialCode")
@@ -80,6 +81,12 @@ public class ProfileActivity extends AppCompatActivity {
             adminView.setChecked(true);
         }
         organizerField = findViewById(R.id.organizerFields);
+        facilityName = findViewById(R.id.facilityName);
+        facilityAddress = findViewById(R.id.facilityAddress);
+        facilityCapacity = findViewById(R.id.facilityCapacity);
+        facilityContact = findViewById(R.id.facilityContact);
+        facilityEmail = findViewById(R.id.facilityEmail);
+        facilityOwner = findViewById(R.id.facilityOwner);
 
         imageUriString = getSharedPreferences("ProfilePrefs", MODE_PRIVATE)
                 .getString("profile_image_uri", null);
@@ -101,6 +108,7 @@ public class ProfileActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     organizerField.setVisibility(View.VISIBLE);
+                    loadFacilityData();
                 } else {
                     organizerField.setVisibility(View.GONE);
                 }
@@ -114,12 +122,20 @@ public class ProfileActivity extends AppCompatActivity {
             String phoneNumber = phoneEditText.getText().toString().trim();
             boolean notificationsChecked = notifications.isChecked();
 
+            String fName = facilityName.getText().toString().trim();
+            String fAddress = facilityAddress.getText().toString().trim();
+            String fCapacity = facilityCapacity.getText().toString().trim();
+            String fContact = facilityContact.getText().toString().trim();
+            String fEmail = facilityEmail.getText().toString().trim();
+            String fOwner = facilityOwner.getText().toString().trim();
+
             if (name.isEmpty()) {
                 Toast.makeText(this, "No UserName Found", Toast.LENGTH_SHORT).show();
             } else if (email.isEmpty()) {
                 Toast.makeText(this, "No Email Found", Toast.LENGTH_SHORT).show();
             } else {
                 saveProfileData(name, phoneNumber, email, notificationsChecked, imageUriString);
+                saveFacilityData(fName, fAddress, fCapacity, fContact, fEmail, fOwner);
             }
         });
 
@@ -158,6 +174,24 @@ public class ProfileActivity extends AppCompatActivity {
                 .addOnFailureListener(e -> Toast.makeText(ProfileActivity.this, "Failed to save profile", Toast.LENGTH_SHORT).show());
     }
 
+    private void saveFacilityData(String name, String address, String capacity, String contactInfo, String
+                                  email, String owner) {
+        loadFacilityData();
+
+        Map<String, Object> facilityProfile = new HashMap<>();
+        facilityProfile.put("facility name", name);
+        facilityProfile.put("address", address);
+        facilityProfile.put("capacity", capacity);
+        facilityProfile.put("contact information", contactInfo);
+        facilityProfile.put("email", email);
+        facilityProfile.put("owner", owner);
+
+        db.collection("FACILITY_PROFILES")
+                .document(USER_ID).set(facilityProfile)
+                .addOnSuccessListener(aVoid -> Toast.makeText(ProfileActivity.this, "Profile saved successfully", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> Toast.makeText(ProfileActivity.this, "Failed to save profile", Toast.LENGTH_SHORT).show());
+    }
+
     private void loadProfileData() {
         DocumentReference docRef = db.collection("USER_PROFILES").document(USER_ID);
         docRef.get()
@@ -174,6 +208,31 @@ public class ProfileActivity extends AppCompatActivity {
                         emailEditText.setText(email != null ? email : "");
                         notifications.setChecked(notificationsChecked);
                         loadProfilePicture(sImageUri);
+                    }
+
+                })
+                .addOnFailureListener(e -> Toast.makeText(ProfileActivity.this, "Failed to load profile", Toast.LENGTH_SHORT).show());
+    }
+
+    private void loadFacilityData() {
+        DocumentReference docRef = db.collection("FACILITY_PROFILES").document(USER_ID);
+
+        docRef.get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        String name = documentSnapshot.getString("facility name");
+                        String address = documentSnapshot.getString("address");
+                        String capacity = documentSnapshot.getString("capacity");
+                        String contactInfo = documentSnapshot.getString("contact information");
+                        String email = documentSnapshot.getString("email");
+                        String owner = documentSnapshot.getString("owner");
+
+                        facilityName.setText(name != null ? name : "");
+                        facilityAddress.setText(address != null ? address : "");
+                        facilityCapacity.setText(capacity != null ? capacity : "");
+                        facilityContact.setText(contactInfo != null ? contactInfo : "");
+                        facilityEmail.setText(email != null ? email : "");
+                        facilityOwner.setText(owner != null ? owner : "");
                     }
 
                 })
