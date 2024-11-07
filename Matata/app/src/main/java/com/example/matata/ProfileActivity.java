acdcd8fa5f9a0f06266188cd3dc4726264132da7
rcli620 <richard164184@gmail.com>
2024-11-06 19:11:41 -0700

    Organizers are automatically add to ORGANIZER_PROFILES

--- Matata/app/src/main/java/com/example/matata/ProfileActivity.java
+++ Matata/app/src/main/java/com/example/matata/ProfileActivity.java
@@ -121,6 +121,7 @@ public class ProfileActivity extends AppCompatActivity {
             String email = emailEditText.getText().toString().trim();
             String phoneNumber = phoneEditText.getText().toString().trim();
             boolean notificationsChecked = notifications.isChecked();
+            boolean organizerChecked = isOrganizer.isChecked();
 
             String fName = facilityName.getText().toString().trim();
             String fAddress = facilityAddress.getText().toString().trim();
@@ -136,7 +137,10 @@ public class ProfileActivity extends AppCompatActivity {
                 Toast.makeText(this, "No Email Found", Toast.LENGTH_SHORT).show();
             } else {
                 saveProfileData(name, phoneNumber, email, notificationsChecked, imageUriString);
-                saveFacilityData(fName, fAddress, fCapacity, fContact, fEmail, fOwner);
+                if (organizerChecked) {
+                    saveFacilityData(fName, fAddress, fCapacity, fContact, fEmail, fOwner);
+                    addToOrganizer(name, phoneNumber, email, notificationsChecked, imageUriString);
+                }
             }
         });
 
@@ -189,6 +193,23 @@ public class ProfileActivity extends AppCompatActivity {
 
         db.collection("FACILITY_PROFILES")
                 .document(USER_ID).set(facilityProfile)
+                .addOnSuccessListener(aVoid -> Toast.makeText(ProfileActivity.this, "Profile saved successfully", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> Toast.makeText(ProfileActivity.this, "Failed to save profile", Toast.LENGTH_SHORT).show());
+    }
+
+    private void addToOrganizer(String name, String phone, String email, boolean notificationsChecked, String imageUriString) {
+
+        loadProfilePicture(imageUriString);
+
+        Map<String, Object> userProfile = new HashMap<>();
+        userProfile.put("username", name);
+        userProfile.put("phone", phone);
+        userProfile.put("email", email);
+        userProfile.put("notifications", notificationsChecked);
+        userProfile.put("profileUri", imageUriString);
+
+        db.collection("ORGANIZER_PROFILES").document(USER_ID)
+                .set(userProfile)
                 .addOnSuccessListener(aVoid -> Toast.makeText(ProfileActivity.this, "Profile saved successfully", Toast.LENGTH_SHORT).show())
                 .addOnFailureListener(e -> Toast.makeText(ProfileActivity.this, "Failed to save profile", Toast.LENGTH_SHORT).show());
     }
