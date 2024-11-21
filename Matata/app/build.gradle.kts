d0a737ee20a4029aaf275fd738fad2c1f30bd6e8
laub141 <brianlau5665@gmail.com>
2024-11-21 10:09:49 -0700

    Dependencies for FCM and FCMToken generation

--- Matata/app/build.gradle.kts
+++ Matata/app/build.gradle.kts
@@ -106,4 +106,5 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation("org.mockito:mockito-android:5.14.2")
+    implementation("com.google.firebase:firebase-messaging")
 }
