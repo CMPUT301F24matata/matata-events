07ffc263a7daf808f2422b411246e5fc713f4543
laub141 <brianlau5665@gmail.com>
2024-11-27 13:07:57 -0700

    Reworking notifications

--- Matata/app/src/main/java/com/example/matata/ViewEvent.java
+++ Matata/app/src/main/java/com/example/matata/ViewEvent.java
@@ -178,23 +178,6 @@ public class ViewEvent extends AppCompatActivity {
                 }
             });
 
-    private void askNotificationPermission() {
-        // This is only necessary for API level >= 33 (TIRAMISU)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ContextCompat.checkSelfPermission(this, POST_NOTIFICATIONS) ==
-                    PackageManager.PERMISSION_GRANTED) {
-                // FCM SDK (and your app) can post notifications.
-            } else if (shouldShowRequestPermissionRationale(POST_NOTIFICATIONS)) {
-                // TODO: display an educational UI explaining to the user the features that will be enabled
-                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
-                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
-                //       If the user selects "No thanks," allow the user to continue without notifications.
-            } else {
-                // Directly ask for the permission
-                requestPermissionLauncher.launch(POST_NOTIFICATIONS);
-            }
-        }
-    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
